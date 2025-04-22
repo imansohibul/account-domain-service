bd8c65e6e0eec3d2e2be08e4915526646a523749
Mochamad Sohibul Iman <iman@imansohibul.my.id>
2025-04-23 01:27:27 +0700

    adjust error

--- entity/error.go
+++ entity/error.go
@@ -3,15 +3,21 @@ package entity
 import "errors"
 
 var (
-	// ErrNotFound is returned when the requested resource is not found
-	ErrNotFound = errors.New("not found")
+	// ErrAccountNotFound is returned when the requested account is not found
+	ErrAccountNotFound = errors.New("Nomor rekening tidak ditemukan")
 
-	// ErrCustomerAlreadyExists is returned when a customer with the same NIK already exists
-	ErrCustomerAlreadyExists = errors.New("NIK sudah terdaftar")
+	// ErrDuplicateAccountNumber is returned when an account with the same account number already exists
+	ErrDuplicateAccountNumber = errors.New("Nomor rekening sudah terdaftar")
+
+	// ErrCustomerNotFound is returned when the requested customer is not found
+	ErrCustomerNotFound = errors.New("Nasabah tidak ditemukan")
 
 	// ErrPhoneNumberAlreadyExists is returned when a customer with the same phone number already exists
 	ErrPhoneNumberAlreadyExists = errors.New("Nomor telepon sudah terdaftar")
 
-	// ErrDuplicateAccountNumber is returned when an account with the same account number already exists
-	ErrDuplicateAccountNumber = errors.New("Nomor rekening sudah terdaftar")
+	// ErrCustomerIdentityNotFound is returned when the requested customer identity is not found
+	ErrCustomerIdentityNotFound = errors.New("Identitas nasabah tidak ditemukan")
+
+	//ErrCustomerIdentityAlreadyExists is returned when a customer with the same NIK already exists
+	ErrCustomerIdentityAlreadyExists = errors.New("NIK sudah terdaftar")
 )
