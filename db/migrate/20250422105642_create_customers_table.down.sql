d79c7c75876600196ef60b1268e2dd1ebc48329c
Mochamad Sohibul Iman <iman@imansohibul.my.id>
2025-04-22 19:00:37 +0700

    create db migration script for create customers table

---
+++ db/migrate/20250422105642_create_customers_table.down.sql
@@ -0,0 +1,2 @@
+-- Drop table customers if exists (rollback migration)
+DROP TABLE IF EXISTS customers;
---
+++ db/migrate/20250422105642_create_customers_table.up.sql
@@ -0,0 +1,14 @@
+-- This SQL script creates a table named 'customers' in the database.
+-- The table is designed to store customer information.
+-- It includes fields for the customer's full name, phone number, and timestamps for creation and updates.
+-- The phone number is standardized to E.164 format for international compatibility.
+-- The table also includes a unique constraint on the phone number to prevent duplicates.
+CREATE TABLE IF NOT EXISTS customers (
+    id BIGSERIAL PRIMARY KEY,                 -- Auto-incrementing ID
+    fullname VARCHAR(255) NOT NULL,        -- Full name of the customer
+    phone_number VARCHAR(16),              -- Phone number of the customer, Standardization (E.164 format - International Compatibility)
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Automatically set creation timestamp
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP  -- Automatically set updated timestamp
+
+    CONSTRAINT uq_customer_phone_number UNIQUE(phone_number)  -- Prevent duplicates
+);
